e60ac985d2e20ce64fba0d8a333324ccbfbd4ec0
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-14 22:44:48 +0100

    chore: removed unused import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
 
-import { Badge, Provider as PaperProvider } from "react-native-paper";
 import Main from "./src/Main";
 
 export default function App() {
