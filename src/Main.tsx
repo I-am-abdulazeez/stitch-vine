7018f9bb6c6a07c56db3f9b3022206d1d22e719a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-14 22:57:56 +0100

    chore: init route in Main.tsx file

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Badge } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "./screens/Home";
+
+import { navigationTheme } from "../assets/theme";
+
+import { StackParams } from "../types";
+
+const Stack = createStackNavigator<StackParams>();
 
 const Main = () => {
   return (
-    <View>
-      <Text>Main</Text>
-      <Badge>4</Badge>
-    </View>
+    <NavigationContainer theme={navigationTheme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
