c80e86bbf04a703e5b1ed73c83a9a564df6fbe3e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-14 22:24:30 +0100

    chore: Main.tsx is from the src folder

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+import { Badge } from "react-native-paper";
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Main</Text>
+      <Badge>4</Badge>
+    </View>
+  );
+};
+
+export default Main;
