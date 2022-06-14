82692784be805e19b9bbff21b37074cee3e2f567
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-14 22:08:15 +0100

    chore: testing react-native-paper component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
+import { Badge } from "react-native-paper";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>App</Text>
+      <Badge>4</Badge>
     </View>
   );
 };
