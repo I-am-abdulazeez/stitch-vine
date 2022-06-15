88265c9d2fe2c4a4cb5cfc3890928d70bca0f54b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-15 08:18:29 +0100

    chore: homescreen structure + styles

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,63 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Button } from "react-native-paper";
+import { BlurView } from "@react-native-community/blur";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { View, StatusBar, ImageBackground } from "react-native";
+
+import { Button, Headline, Title } from "react-native-paper";
+import { StackParams } from "../../types";
+
+import { styles } from "../styles";
+
+const imageSrc = require("../../assets/images/landing-bg.jpg");
 
 const HomeScreen = () => {
+  const navigation = useNavigation<StackNavigationProp<StackParams>>();
   return (
-    <View style={styles.container}>
-      <Text>Surface</Text>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log("Pressed")}
+    <>
+      <StatusBar translucent backgroundColor={"transparent"} />
+      <ImageBackground
+        resizeMode="cover"
+        style={styles.imageStyle}
+        source={imageSrc}
       >
-        Press me
-      </Button>
-    </View>
+        <View style={styles.panel}>
+          <Headline style={styles.lgText}>Join the best</Headline>
+          <Headline style={styles.lgTextTwo}>tailoring app</Headline>
+          <View style={{ marginVertical: 30 }}>
+            <Title style={styles.secText}>
+              Take your tailoring to the next level
+            </Title>
+            <Title
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontFamily: "Inter_500Medium",
+                marginTop: -3,
+                fontSize: 18,
+              }}
+            >
+              with Stitch Vine
+            </Title>
+          </View>
+          <View style={{ paddingHorizontal: 30 }}>
+            <Button
+              onPress={() => navigation.navigate("Login")}
+              uppercase={false}
+              theme={{
+                roundness: 10,
+              }}
+              labelStyle={{ fontSize: 18, fontFamily: "Inter_600SemiBold" }}
+              contentStyle={{ height: 55 }}
+              mode="contained"
+              compact
+            >
+              Get Started
+            </Button>
+          </View>
+        </View>
+      </ImageBackground>
+    </>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-});
